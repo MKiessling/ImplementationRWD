1b03480dc6c438b1083c1fb6624f4eab994ac643
MKiessling <kiesslingmartin91@googlemail.com>
2014-05-01 16:12:10 +0200

    fixing da shit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,39 @@
-/* Author:
+$(document).ready(function(){
+    if ($(window).width() <= 759) {
 
-*/
+        var linkedList = new DLL.DoublyLinkedList();
+        var articles = $("article");
+        $.each(articles, function(index, value){
+             linkedList.append(articles[index]);
+            $(articles[index]).hide();
+        });
 
+        var current = linkedList.head();
 
+        $(current.data).show();
 
+        $(document).keyup(function(keyEvent) {
+            if(keyEvent.which ==  39){
+                $(current.data).hide();
+                if(current.next == null){
+                    current = linkedList.head();
+                }else{
+                    current = current.next;
+                }
 
+                $(current.data).show();
+            }
 
+            if(keyEvent.which == 37){
+                $(current.data).hide();
+                if(current.prev == null){
+                    current = linkedList.tail();
+                }else{
+                    current = current.prev;
+                }
+                $(current.data).show();
+            }
+
+        });
+    }
+});
