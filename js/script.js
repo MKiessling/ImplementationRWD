956dbbe21f779de9ad3d36aa2d7c41ef1abad5a2
tr <tobias.roeding@gmail.com>
2014-05-01 16:00:40 +0200

    init

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
-    if ($(window).width() <= 759) {
-
         var linkedList = new DLL.DoublyLinkedList();
         var articles = $("article");
         $.each(articles, function(index, value){
              linkedList.append(articles[index]);
             $(articles[index]).hide();
+            $(articles[index]).find("section").hide();
         });
 
         var current = linkedList.head();
@@ -13,8 +12,20 @@ $(document).ready(function(){
         $(current.data).show();
 
         $(document).keyup(function(keyEvent) {
+            if(keyEvent.which == 37){
+                $(current.data).hide();
+                $(current.data).find("section").hide();
+                if(current.prev == null){
+                    current = linkedList.tail();
+                }else{
+                    current = current.prev;
+                }
+                $(current.data).show();
+            }
+
             if(keyEvent.which ==  39){
                 $(current.data).hide();
+                $(current.data).find("section").hide();
                 if(current.next == null){
                     current = linkedList.head();
                 }else{
@@ -24,16 +35,13 @@ $(document).ready(function(){
                 $(current.data).show();
             }
 
-            if(keyEvent.which == 37){
-                $(current.data).hide();
-                if(current.prev == null){
-                    current = linkedList.tail();
-                }else{
-                    current = current.prev;
-                }
-                $(current.data).show();
+            if(keyEvent.which == 38){
+                $(current.data).find("section").hide();
             }
 
+            if(keyEvent.which == 40){
+                $(current.data).find("section").show();
+            }
         });
-    }
+
 });
