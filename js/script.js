4af226bfc77f25161708c25c008d2be9cb9dd0c6
Tobias <tobias.roeding@gmail.com>
2014-05-02 10:34:36 +0200

    added subheadings

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ $(document).ready(function () {
 
     var articles = $("article");
     var isSmall, isBig = false;
+    var listener = null;
 
+    /* attach EventListener to window to monitor changes in width */
     window.addEventListener('resize', function () {
         if ($(window).width() <= 759 && isBig) {
             small();
@@ -12,16 +14,19 @@ $(document).ready(function () {
         }
     }, true);
 
+    /* triggers if window is big enough for non-mobile version */
     var big = function () {
         isBig = true;
         isSmall = false;
         $.each(articles, function (index, value) {
             $(articles[index]).show();
         });
-        window.removeEventListener('keyup', arguments.callee, false);
 
-    };
+        window.removeEventListener('keyup', listener, false);
 
+    }
+
+    /* triggers if window is small enough for mobile version */
     var small = function () {
         isSmall = true;
         isBig = false;
@@ -29,44 +34,52 @@ $(document).ready(function () {
         $.each(articles, function (index, value) {
             linkedList.append(articles[index]);
             $(articles[index]).hide();
+            $(articles[index]).find("section").hide();
         });
         var current = linkedList.head();
         $(current.data).show();
-        window.addEventListener('keyup', function (e) {
-            if(e.which == 37){
-                $(current.data).hide();
-                $(current.data).find("section").hide();
-                if(current.prev == null){
-                    current = linkedList.tail();
-                }else{
-                    current = current.prev;
-                }
-                $(current.data).show();
-            }
 
-            if(e.which ==  39){
+        listener = function (keyEvent) {
+            /* Right-Arrow */
+            if (keyEvent.keyCode === 39) {
                 $(current.data).find("section").hide();
-                $(current.data).hide("slide");
-
-                if(current.next == null){
+                $(current.data).effect('slide', {direction: 'left', mode: 'hide', distance: '100%'}, 1000);
+                if (current.next == null) {
                     current = linkedList.head();
-                }else{
+                } else {
                     current = current.next;
                 }
 
-                $(current.data).show("slide");
+                $(current.data).effect('slide', {direction: 'right', mode: 'show', distance: '100%'}, 1000);
+
+            }
+            /* Left-Arrow */
+            if (keyEvent.keyCode === 37) {
+                $(current.data).find("section").hide();
+                $(current.data).effect('slide', {direction: 'right', mode: 'hide', distance: '100%'}, 1000);
+                if (current.prev == null) {
+                    current = linkedList.tail();
+                } else {
+                    current = current.prev;
+                }
+                $(current.data).effect('slide', {direction: 'left', mode: 'show', distance: '100%'}, 1000);
             }
 
-            if(e.which == 38){
+            /* Up-Arrow */
+            if(keyEvent.which === 38){
                 $(current.data).find("section").slideUp();
             }
 
-            if(e.which == 40){
+            /* Down-Arrow */
+            if(keyEvent.which === 40){
                 $(current.data).find("section").slideDown();
             }
-        }, false);
+        };
+
+        window.addEventListener('keyup', listener, false);
     };
 
+    /* is triggered once the page was loaded */
     if ($(window).width() <= 759) {
         small();
     }
